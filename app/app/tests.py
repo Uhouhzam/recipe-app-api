7ce07444e2a52e19ee58640da0d2fdd79d2f3fe5
Mazhou <mazhouhu97@gmail.com>
2025-03-15 14:23:08 +0100

    fix bug linté

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,10 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
+
 
     def test_add_numbers(self):
         """Test add numbers together."""
@@ -15,8 +17,10 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
+
     def test_subtract_numbers(self):
         """Test substrcting numbers."""
 
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
+        
